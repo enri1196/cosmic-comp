591851165571175aaddff71401e23072645ecb3f
enri1196 <github@snowless.it>
2026-01-04 15:29:33 +0100

    refactor(systemd): use systemd dbus call

--- src/systemd.rs
+++ src/systemd.rs
@@ -1,35 +1,47 @@
 // SPDX-License-Identifier: GPL-3.0-only
 
 use crate::state::Common;
-use libsystemd::daemon::{NotifyState, booted, notify};
-use std::process::Command;
+use libsystemd::daemon::{booted, notify, NotifyState};
 use tracing::{error, warn};
+use zbus::{blocking::Connection, Result};
+
+#[zbus::proxy(
+    interface = "org.freedesktop.systemd1.Manager",
+    default_service = "org.freedesktop.systemd1",
+    default_path = "/org/freedesktop/systemd1",
+    gen_blocking = true
+)]
+trait Manager {
+    fn set_environment(&self, assignments: &[&str]) -> zbus::Result<()>;
+}
 
 pub fn ready(common: &Common) {
-    if booted() {
-        match Command::new("systemctl")
-            .args(["--user", "import-environment", "WAYLAND_DISPLAY", "DISPLAY"])
-            .env("WAYLAND_DISPLAY", &common.socket)
-            .env(
-                "DISPLAY",
-                common
-                    .xwayland_state
-                    .as_ref()
-                    .map(|s| format!(":{}", s.display))
-                    .unwrap_or_default(),
-            )
-            .status()
-        {
-            Ok(x) if x.success() => {}
-            Ok(x) => warn!(
-                exit_code = ?x.code(),
-                "Failed to import WAYLAND_DISPLAY/DISPLAY into systemd",
-            ),
-            Err(err) => error!(?err, "Failed to run systemctl although booted with systemd",),
-        };
-
-        if let Err(err) = notify(false, &[NotifyState::Ready]) {
-            error!(?err, "Failed to notify systemd");
-        }
+    if !booted() {
+        return;
     }
+
+    if let Err(err) = set_systemd_environment(common) {
+        warn!(?err, "Failed to import WAYLAND_DISPLAY/DISPLAY into systemd via D-Bus");
+    }
+
+    if let Err(err) = notify(false, &[NotifyState::Ready]) {
+        error!(?err, "Failed to notify systemd that service is ready");
+    }
+}
+
+fn set_systemd_environment(common: &Common) -> Result<()> {
+    let connection = Connection::session()?;
+    let proxy = ManagerProxyBlocking::new(&connection)?;
+
+    let mut env_vars = vec![format!("WAYLAND_DISPLAY={}", &common.socket.to_str().unwrap_or_default())];
+
+    if let Some(s) = common.xwayland_state.as_ref() {
+        env_vars.push(format!("DISPLAY=:{}", s.display));
+    }
+
+    let env_vars_ref: Vec<&str> = env_vars.iter().map(|s| s.as_str()).collect();
+
+    proxy.set_environment(&env_vars_ref)?;
+
+    Ok(())
 }
